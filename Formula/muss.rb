1632769d0992a0c2bec4065045bab70422089c8f
Randy Stauner <rstauner@instructure.com>
2020-05-26 10:48:35 -0700

    Use Hardward::CPU.arch to avoid deprecation and backticks
    Change-Id: I46b4e9fd1b71d0ad8ad88fd62dc68a89d4103120
    Reviewed-on: https://gerrit.instructure.com/c/homebrew-muss/+/238449
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Josh Gramoll <jgramoll@instructure.com>
    QA-Review: Randy Stauner <rstauner@instructure.com>
    Product-Review: Randy Stauner <rstauner@instructure.com>

--- Formula/muss.rb
+++ Formula/muss.rb
@@ -6,7 +6,7 @@ class Muss < Formula
   end
 
   def self.go_arch
-    arch = OS.mac? ? ::MacOS.preferred_arch.to_s : `uname -m`.chomp
+    arch = ::Hardware::CPU.arch
     case arch
     when "x86_64"
       "amd64"
